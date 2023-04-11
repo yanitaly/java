727b90cde457fd96c233416a0f13629126d8bfa9
yanitaly <44038346+yanitaly@users.noreply.github.com>
2023-04-11 18:22:38 +0200

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,9 @@
+// program to print 'Hello World'
+// javac helloworld.java
+// java HELLOWORLD
+
 class HELLOWORLD {
-public static void main(String args[]){
-    System.out.println("Hello World");
+ public static void main(String args[]){
+     System.out.println("Hello World");
  }
 }
